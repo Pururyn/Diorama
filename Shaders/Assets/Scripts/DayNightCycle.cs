52f9bcf65be8f5bba309861d119f56482007d57e
Coralie Thiery <cocosteph0312@gmail.com>
2025-11-12 15:03:33 +0100

    cycle day/night

---
+++ Shaders/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField] private float dayLengthSeconds = 60f;
+    [SerializeField] private float timeOfDay = 0f;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] public Gradient skyColorGradient;
+
+    private Light directionalLight; 
+
+    void Start()
+    {
+        directionalLight = GetComponent<Light>();   
+        rotationSpeed = 360f / dayLengthSeconds; //degreees per seconds
+    }
+
+    
+    void Update()
+    {
+        // increment time
+        timeOfDay += Time.deltaTime / dayLengthSeconds; 
+        timeOfDay %= 1f;
+
+        // rotate light
+        transform.rotation = Quaternion.Euler((timeOfDay * 360f) - 90,0,0); 
+
+        // set Ambient Light Color
+        RenderSettings.skybox.SetColor("_Tint", skyColorGradient.Evaluate(timeOfDay));
+    }
+}
