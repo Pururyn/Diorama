c88fdbff8318bd6e66ee43b5b8f0cf31e7043172
Coralie Thiery <cocosteph0312@gmail.com>
2025-11-13 21:45:32 +0100

    modif

--- Shaders/Assets/Scripts/DayNightCycle.cs
+++ Shaders/Assets/Scripts/DayNightCycle.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    [SerializeField] private float dayLengthSeconds = 60f;
-    [SerializeField] private float timeOfDay = 0.5f;
-    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float dayLengthSeconds = 40f;
+    [SerializeField, Range(0f, 1f)] private float timeOfDay = 0.5f;
     [SerializeField] public Gradient skyColorGradient;
+    [SerializeField] private Gradient lightColorGradient;
 
-    private Light directionalLight; 
+    private Light directionalLight;
+    private float rotationSpeed;
 
     void Start()
     {
@@ -18,14 +19,29 @@ public class DayNightCycle : MonoBehaviour
     
     void Update()
     {
+        if (directionalLight == null)
+            return;
+
         // increment time
         timeOfDay += Time.deltaTime / dayLengthSeconds; 
         timeOfDay %= 1f;
 
         // rotate light
-        transform.rotation = Quaternion.Euler((timeOfDay * 360f) - 90,0,0); 
+        transform.rotation = Quaternion.Euler((timeOfDay * 360f) - 90f, 0f ,0f);
 
-        // set Ambient Light Color
-        RenderSettings.skybox.SetColor("_Tint", skyColorGradient.Evaluate(timeOfDay));
+        //update light color + intensity
+        directionalLight.color = lightColorGradient.Evaluate(timeOfDay);
+
+        //update sky color
+
+        RenderSettings.skybox.SetColor("Tint", skyColorGradient.Evaluate(timeOfDay));
+
+        Shader.SetGlobalVector("LightDirection", -directionalLight.transform.forward);
+        Shader.SetGlobalColor("LightColor", directionalLight.color * directionalLight.intensity);
+
+        float nightBlend = Mathf.Clamp01(Mathf.Abs(Vector3.Dot(Vector3.down, directionalLight.transform.forward)));
+        Shader.SetGlobalFloat("NightBlend", nightBlend); 
+
+        
     }
 }
